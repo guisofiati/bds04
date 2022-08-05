39a98f61992ca1391035a06203b985333c333386
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-08-05 16:36:21 -0300

    Testes OK

---
+++ src/main/java/com/devsuperior/bds04/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.devsuperior.bds04.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> fieldsValidation(MethodArgumentNotValidException e, HttpServletRequest req) {
+		ValidationError err = new ValidationError();
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Field validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(req.getRequestURI());
+		
+		for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+			err.addError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
+}
